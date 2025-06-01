e1ed50358a7d41fd884f5cfcc6ad089773cfb74b
Ivan Pletnyov <ivan.pletnyov@transoftgroup.com>
2025-06-01 23:58:09 +0300

    Les 1: Pages

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Главная страница</h1>
+    <NuxtLink to="/about">Перейти на About</NuxtLink> |
+    <NuxtLink :to="`/user/42`">Открыть профиль юзера 42</NuxtLink>
+  </div>
+</template>
---
+++ pages/product/[slug].vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>This is product page info:</h1>
+  <p>Название товара: {{ slug }}</p>
+  <p>Реферал: {{ ref }}</p>
+  <p>Источник: {{ utmSource }}</p>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+const slug = computed(() => route.params.slug as string)
+const ref = computed(() => route.query.ref as string || '-')
+const utmSource = computed(() => route.query.utm_source as string || '-')
+</script>
