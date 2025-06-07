6733bc1c58bfda7a9bfbbfe073216a088953af6e
Ivan Pletnyov <ivan.pletnyov@transoftgroup.com>
2025-06-07 22:32:49 +0300

    Les 5: State and reactive

---
+++ components/Components/TextInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <input
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      class="border px-2 py-1"
+  />
+</template>
+
+<script setup lang="ts">
+defineProps<{ modelValue: string }>()
+const emit = defineEmits<{
+  (event: 'update:modelValue', value: string): void
+}>()
+</script>
--- pages/users/[id].vue
+++ pages/users/[id].vue
@@ -9,10 +9,22 @@
     <div>Email: {{ currentUser.email }}</div>
   </div>
   <BackLink :return-link="'/'" :return-title="'На главную'" />
+
+  <button @click="counter++">Clicked {{ counter }} times</button>
+
+  <div>
+    <input v-model="name" />
+    <p>Привет, {{ name }}!</p>
+  </div>
 </div>
+
+<TextInput v-model="username" />
+<p>Текущий логин: {{ username }}</p>
 </template>
 
 <script setup lang="ts">
+import TextInput from "~/components/Components/TextInput.vue";
+
 interface User {
   id: number
   firstName: string
@@ -28,5 +40,20 @@ const currentUser = data.value!
 const fullName = computed(() => {
   if (!data.value) return ''
   return `${data.value.firstName} ${data.value.lastName}`
+})
+
+const counter = ref(0)
+const name = ref('adawd')
+console.log(name)
+console.log(name.value)
+
+const user = reactive({ firstName: 'Ivan', lastName: 'Petrov' })
+
+const fullNameComputedExample = computed(() => `${user.firstName} ${user.lastName}`)
+
+const username = ref('')
+
+watchEffect(() => {
+  console.log('username ' + username.value + ' changed.')
 })
 </script>
